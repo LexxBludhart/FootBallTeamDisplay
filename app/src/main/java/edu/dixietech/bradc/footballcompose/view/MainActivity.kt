77d63b89d8815e80be8a1265226238107cc0b23e
Brad Campbell <bradc@dixietech.edu>
2023-03-02 10:46:51 -0700

    Opt in to experimental apis

--- app/src/main/java/edu/dixietech/bradc/footballcompose/view/MainActivity.kt
+++ app/src/main/java/edu/dixietech/bradc/footballcompose/view/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.material3.ExperimentalMaterial3Api
 import edu.dixietech.bradc.footballcompose.view_model.FootballViewModel
 
 class MainActivity : ComponentActivity() {
     private val viewModel by viewModels<FootballViewModel>()
 
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
