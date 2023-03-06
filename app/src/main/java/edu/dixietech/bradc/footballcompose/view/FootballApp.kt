25c3766a522224d0ee418f28fdb1966277dcb2fd
Brad Campbell <bradc@dixietech.edu>
2023-03-06 11:03:09 -0700

    Add navigation support to the App

--- app/src/main/java/edu/dixietech/bradc/footballcompose/view/FootballApp.kt
+++ app/src/main/java/edu/dixietech/bradc/footballcompose/view/FootballApp.kt
@@ -1,48 +1,82 @@
 package edu.dixietech.bradc.footballcompose.view
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowBack
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight.Companion.Bold
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import edu.dixietech.bradc.footballcompose.R
+import edu.dixietech.bradc.footballcompose.navigation.FootballNavHost
 import edu.dixietech.bradc.footballcompose.ui.theme.FootballComposeTheme
 import edu.dixietech.bradc.footballcompose.view_model.FootballViewModel
 
+@ExperimentalMaterial3Api
 @Composable
 fun FootballApp(viewModel: FootballViewModel, modifier: Modifier = Modifier) {
     FootballComposeTheme {
-        Surface(modifier = modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .fillMaxSize()
-                    .verticalScroll(rememberScrollState())
-            ) {
-                viewModel.league.keys.forEach { divisionName ->
-                    val teamList = viewModel.league[divisionName]
-                    Text(
-                        divisionName,
-                        modifier = Modifier.align(CenterHorizontally),
-                        fontSize = 36.sp,
-                        fontWeight = Bold
-                    )
+        val navController = rememberNavController()
+        val currentBackStack by navController.currentBackStackEntryAsState()
+        val currentDestination = currentBackStack?.destination
+        val coroutineScope = rememberCoroutineScope()
 
-                    teamList?.forEach { team ->
-                        ListItem(team)
-                    }
-
-                    Spacer(Modifier.size(16.dp))
-                }
-            }
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = {
+                        Text(
+                            text = "Football is life!",
+                            color = Color.White
+                        )
+                    },
+                    navigationIcon = {
+                        if (navController.previousBackStackEntry != null) {
+                            IconButton(onClick = {
+                                navController.navigateUp()
+                            }) {
+                                Icon(Icons.Rounded.ArrowBack, stringResource(R.string.content_description_back), tint = MaterialTheme.colorScheme.onPrimary)
+                            }
+                        }
+                    },
+                    colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = MaterialTheme.colorScheme.primary),
+                )
+            },
+            ) { innerPadding ->
+            FootballNavHost(navController, viewModel, Modifier.padding(innerPadding))
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar(controller: NavHostController) {
+    TopAppBar(
+        title = {
+            Text(stringResource(id = R.string.app_name), color = MaterialTheme.colorScheme.onPrimary)
+        },
+        colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = MaterialTheme.colorScheme.primary),
+        navigationIcon = {
+            if (controller.previousBackStackEntry != null) {
+                IconButton(onClick = {
+                    controller.navigateUp()
+                }) {
+                    Icon(Icons.Rounded.ArrowBack, stringResource(id = R.string.content_description_back), tint = MaterialTheme.colorScheme.onPrimary)
+                }
+            }
+        },
+//        actions = {
+//            IconButton(onClick = { controller.navigate(Help.route) }) {
+//                Icon(Icons.Outlined.Info, stringResource(R.string.content_description_help), tint = MaterialTheme.colorScheme.onPrimary)
+//            }
+//        }
+    )
 }
 
 //@Composable
