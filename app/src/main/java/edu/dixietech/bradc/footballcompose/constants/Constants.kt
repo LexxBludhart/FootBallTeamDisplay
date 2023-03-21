596a29769f0237a47490095b1a014aa96ed925bf
Brad Campbell <bradc@dixietech.edu>
2023-03-21 17:54:42 -0600

    Add nfl colors

---
+++ app/src/main/java/edu/dixietech/bradc/footballcompose/constants/Constants.kt
@@ -0,0 +1,8 @@
+package edu.dixietech.bradc.footballcompose.constants
+
+import androidx.compose.ui.graphics.Color
+
+object Constants {
+    val nflBlue = Color(0xFF013369)
+    val nflRed = Color(0xFFD50A0A)
+}
