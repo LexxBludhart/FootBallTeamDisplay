6f2c182f7cd4a088729cd92f67ac2f342f7bef7a
Brad Campbell <bradc@dixietech.edu>
2023-03-23 10:36:20 -0600

    Add fillMaxSize to top level composable and pass it down through the chain

--- app/src/main/java/edu/dixietech/bradc/footballcompose/view/MainActivity.kt
+++ app/src/main/java/edu/dixietech/bradc/footballcompose/view/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.ui.Modifier
 import edu.dixietech.bradc.footballcompose.view_model.FootballViewModel
 
 class MainActivity : ComponentActivity() {
@@ -14,14 +16,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            FootballApp(viewModel = viewModel)
+            FootballApp(
+                viewModel = viewModel,
+                modifier = Modifier.fillMaxSize()
+            )
         }
     }
 }
-
-//@Preview(showBackground = true)
-//@Composable
-//fun DefaultPreview() {
-//    FootballComposeTheme {
-//    }
-//}
