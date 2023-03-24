350998a0222893e2bc376c0eb3db3342b5b6f735
Brad Campbell <bradc@dixietech.edu>
2023-03-24 16:39:50 -0600

    Implement the bulk of the DetailScreen

--- app/src/main/java/edu/dixietech/bradc/footballcompose/view/DetailScreen.kt
+++ app/src/main/java/edu/dixietech/bradc/footballcompose/view/DetailScreen.kt
@@ -1,68 +1,83 @@
 package edu.dixietech.bradc.footballcompose.view
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawWithCache
 import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.graphics.TileMode
+import androidx.compose.ui.geometry.center
+import androidx.compose.ui.graphics.*
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import edu.dixietech.bradc.footballcompose.model.Team
+import coil.compose.rememberAsyncImagePainter
 import edu.dixietech.bradc.footballcompose.view_model.FootballViewModel
+
 
 @Composable
 fun DetailScreen(
     viewModel: FootballViewModel,
-    teamId: Int?
+    teamId: Int?,
+    modifier: Modifier
 ) {
     val team = viewModel.nflTeams.first { it.id == teamId }
 
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        ) {
+    Surface(modifier = modifier) {
         GradientView(colorValues = team.colors)
-        Spacer(modifier = Modifier)
-        Text(
-            team.name,
-            textAlign = TextAlign.Center,
-            color = Color.White,
-            fontSize = 36.sp,
-            fontWeight = FontWeight.Bold
-        )
-        Spacer(modifier = Modifier)
 
+        // Try without column first...
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            Image(
+                painter = rememberAsyncImagePainter(team.logo),
+                contentDescription = "nfl team image",
+                modifier = Modifier
+                    .wrapContentSize()
+                    .wrapContentHeight()
+                    .wrapContentWidth()
+            )
+
+            Text(
+                team.name,
+                textAlign = TextAlign.Center,
+                color = Color.White,
+                fontSize = 36.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
     }
-
-//        .background(
-//            brush = Brush.verticalGradient(
-//                colors = item.colors.map { Color(it) }
-//            )
-//        )
-
 }
 
 @Composable
-fun GradientView(modifier: Modifier = Modifier, colorValues: Array<Long>) {
+fun GradientView(colorValues: Array<Long>) {
     val colors = colorValues.map { Color(it) }
+
+    val largeRadialGradient = object : ShaderBrush() {
+        override fun createShader(size: Size): Shader {
+            val biggerDimension = maxOf(size.height, size.width)
+            return RadialGradientShader(
+                colors = colors,
+                center = size.center,
+                radius = biggerDimension / 2f,
+//                colorStops = listOf(0f, 0.95f)
+            )
+        }
+    }
 
 //    val parentSize = parentLayoutCoordinates?.size?.toSize()?: Size.Zero
 
-    Box(modifier = modifier.diagonalGradientBorder(colors, 300.0F)) // TODO: Parent Width???
+//    Box(modifier = modifier.diagonalGradientBorder(colors, 300.0F)) // TODO: Parent Width???
+    Box(modifier = Modifier.background(largeRadialGradient)
+    )
 }
 
 fun Modifier.diagonalGradientBorder(
@@ -74,7 +89,7 @@ fun Modifier.diagonalGradientBorder(
         colors,
         startX = -offset,
         endX = width - offset,
-        tileMode = TileMode.Mirror
+        tileMode = TileMode.Repeated
     )
 )
 
